cd92eefcf88cc6b046c4e0f5d79a5a0c2b169291
gzy <1522780701@qq.com>
2018-03-12 10:34:31 +0800

    数据库同步问题修改
    应该是audioId

--- weixin/migrations/0004_auto_20180129_1122.py
+++ weixin/migrations/0004_auto_20180129_1122.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='orders',
-            name='videoId',
+            name='audioId',
             field=models.CharField(blank=True, max_length=256),
         ),
         migrations.AlterField(
